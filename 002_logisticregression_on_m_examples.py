d52ed9a8f6962d9b4e1acf1c2786a7c092bbbf96
Your Name <you@example.com>
2017-11-15 15:57:25 -0200

    vai

--- 002_logisticregression_on_m_examples.py
+++ 002_logisticregression_on_m_examples.py
@@ -1,34 +1,32 @@
 import numpy as np
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
 def loss(a, y):
     return -(y * np.log(a) + (1 - y) * np.log(1 - a))
 
 
-def cost_function(a, y):
-    i = loss(a, y)
-    summmed = np.sum(i)
-    return summmed / len(a)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-def my_function(X, Y, W, b):
+def compute_gradient(X, Y, W, b):
     J = 0; dw1 = 0; dw2 = 0; db = 0
     m = X.shape[1]
 
-    for i in range(m):
+    for i in range(0, m):
         xi = X[:,i]
         yi = Y[i]
-        zi = xi.dot(W) + b
+        zi = W.dot(xi) + b
         ai = sigmoid(zi)
         J += loss(ai, yi)
         dzi = ai - yi
         dw1 += xi[0] * dzi
         dw2 += xi[1] * dzi
         db += dzi
+
+    J /= m; dw1 /=m; dw2 /=m; db /= m
+
+    return {'cost': J, 'weight derivative': np.array([dw1, dw2]), 'bias derivative': db}
 
 
 if __name__ == '__main__':
@@ -38,8 +36,14 @@ if __name__ == '__main__':
 
     X = np.array([point1, point2, point3]).T
     Y = np.array([1, 1, 0])
+    W = np.array([.5, -.5])
+    b = .3
 
-    W = np.array([.5, .3])
-    b = .2
+    gradient = compute_gradient(X, Y, W, b)
+    print(gradient)
 
-    my_function(X, Y, W, b)
+    # TODO fazer um gradient checking para ver se esse cara funciona direito.
+
+    # TODO fazer isso com vetorization.
+
+    # TODO rever o grafo de deep learning para entender direitinho o algoritmo
